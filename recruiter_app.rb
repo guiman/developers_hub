3b26f503e6126b0a3ca441f3a19ad5ba252ec4b2
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-03-12 22:44:54 +0000

    Filter for the top 3 languages and don't mind people not available for hire

--- recruiter_app.rb
+++ recruiter_app.rb
@@ -16,8 +16,11 @@ Octokit.middleware = stack
 class RecruiterApp < Sinatra::Base
   get '/' do
     @search = Recruiter.search(cached: true).at("Portsmouth").and_at("Southampton").and_at("Winchester").with_repos('>5').skills("Ruby,Javascript")
-    candidates = @search.all.slice(1..20)
-    @candidates = candidates.select { |candidate| candidate.hireable && (candidate.languages.keys.include?(:Ruby) || candidate.languages.keys.include?(:JavaScript)) }
+    candidates = @search.all.slice(1..30)
+    @candidates = candidates.select do |candidate|
+      languages = candidate.skills.top(3)
+      candidate.location.include?("UK") && languages.include?(:Ruby) && languages.include?(:JavaScript)
+    end
     erb :index
   end
 end
