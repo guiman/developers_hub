4f9e932de63e6c20640923248678b2e64fa2f346
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-03-20 08:31:19 +0000

    Remove unused method and update recruiter gem

--- lib/recruiter_extensions/indexed_user.rb
+++ lib/recruiter_extensions/indexed_user.rb
@@ -7,28 +7,5 @@ module RecruiterExtensions
     def ==(another_object)
       login == another_object.login
     end
-
-    def standarized_location
-      # remove repeated spaces
-      address_parts = location.split.join(' ')
-
-      if address_parts.split(',').count >= 2
-        # Southmapton, hampshire
-        address_parts = address_parts.split(',')
-      else
-        # Southmapton hampshire
-        address_parts = address_parts.split(' ')
-      end
-
-      # Southmapton hampshire uk
-      if address_parts.count > 2
-        address_parts = address_parts.first(2)
-      end
-
-      address_parts = address_parts.map { |address_part| address_part.gsub(/ /, '') }.join(',')
-
-      # hampshire,uk
-      address_parts.downcase.gsub(/england/, 'uk').gsub(/unitedkingdom/, 'uk')
-    end
   end
 end
