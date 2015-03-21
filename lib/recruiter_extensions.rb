bff719f4a73a754d13168a69a0d41d6389f85b5e
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-03-21 18:37:08 +0000

    Implement URL filtering
    Create better more descriptive URLs and concentrate filtering logic into
    independent module RecruiterExtensions::FilterIndexedUsers which takes care of
    building correct search in the database.

--- lib/recruiter_extensions.rb
+++ lib/recruiter_extensions.rb
@@ -1,6 +1,7 @@
 require 'recruiter_extensions/indexed_user'
 require 'recruiter_extensions/github_search_index_updater'
 require 'recruiter_extensions/language_statistics_by_location'
+require 'recruiter_extensions/filter_indexed_users'
 
 module RecruiterExtensions
 
--- recruiter_app.rb
+++ recruiter_app.rb
@@ -9,30 +9,51 @@ class RecruiterApp < Sinatra::Base
     erb :index
   end
 
+  # JSON map data for particular languages
   get '/map_data/:lang' do
     content_type :json
 
     RecruiterExtensions::LanguageStatisticsByLocation.new(params.fetch("lang", "all")).perform.to_json
   end
 
-  get '/:lang' do
-    @lang = params.fetch("lang", "all")
-    @candidates = RecruiterExtensions::IndexedUser.where(hireable: true).order(:name).all
-    @candidates = (@lang == "all") ? @candidates : @candidates.select do |candidate|
-        candidate.languages.keys.map(&:to_s).map(&:downcase).include?(@lang)
-      end
+  ### Filters
+  # By location
+  get '/location/:location' do
+    @location = params.fetch("location", "all")
+    @language = params.fetch("language", "all")
+    @candidates = RecruiterExtensions::FilterIndexedUsers.new(
+      location: @location).all
 
     erb :map
   end
 
-  get '/:lang/:lat/:lng' do
-    pair = "#{params.fetch("lat")},#{params.fetch("lng")}"
-    @lang = params.fetch("lang", "all")
+  # By language
+  get '/lang/:language' do
+    @language= params.fetch("language", "all")
+    @candidates = RecruiterExtensions::FilterIndexedUsers.new(
+      language: @language).all
 
-    @candidates = RecruiterExtensions::IndexedUser.where(geolocation: pair).where(hireable: true).order(:name).all
-    @candidates = (@lang == "all") ? @candidates : @candidates.select do |candidate|
-        candidate.languages.keys.map(&:to_s).map(&:downcase).include?(@lang)
-      end
+    erb :map
+  end
+
+  # By location and language
+  get '/lang/:language' do
+    @location = params.fetch("location", "all")
+    @language = params.fetch("language", "all")
+    @candidates = RecruiterExtensions::FilterIndexedUsers.new(
+      location: @location, language: @language).all
+
+    erb :map
+  end
+
+  # By language and geolocation
+  get '/:lat/:lng/:language' do
+    pair = "#{params.fetch("lat")},#{params.fetch("lng")}"
+    @language = params.fetch("language", "all")
+
+    @candidates = RecruiterExtensions::FilterIndexedUsers.new(
+      language: @language, geolocation: pair).all
+
     erb :map
   end
 end
