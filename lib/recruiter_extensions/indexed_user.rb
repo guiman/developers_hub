ef13dd354dd5fc99b94d9e676171e463de822521
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-03-23 06:59:29 +0000

    Obfuscating names with style

--- lib/recruiter_extensions/indexed_user.rb
+++ lib/recruiter_extensions/indexed_user.rb
@@ -4,6 +4,15 @@ module RecruiterExtensions
   class IndexedUser < ActiveRecord::Base
     serialize :languages
 
+    def obfuscated_name
+      names  = name.split(' ')
+      surname = names.pop
+      replacement_lenth = surname.size - 5 % surname.size
+      to_be_replaced = surname.slice(surname.size - replacement_lenth, surname.size)
+      surname = surname.sub(to_be_replaced, '*' * replacement_lenth)
+      names.push(surname).join ' '
+    end
+
     def sorted_languages
       languages.sort {|a,b| b[1] <=> a[1] }.to_h
     end
