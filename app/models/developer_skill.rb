2a2c663ba5ac639f71488e6bd9636d3d2e8665aa
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-07-13 21:26:59 +0100

    Extract logic for skill creating based on candidate languages

--- app/models/developer_skill.rb
+++ app/models/developer_skill.rb
@@ -4,4 +4,22 @@ class DeveloperSkill < ActiveRecord::Base
 
   validates :developer, presence: true
   validates :skill, presence: true
+
+  # Create set of skills using candidate languages as origin
+  def self.create_from_candidate(developer:, candidate:)
+    candidate.languages.each do |language, repos|
+      repos = repos.map(&:symbolize_keys)
+      skill = Skill.find_or_create_by(name: language.to_s)
+      sorted_repos = repos.sort { |a,b| b[:popularity] <=> a[:popularity] }
+      top_skill_repo = sorted_repos.detect { |repo| repo.fetch(:main_language).to_s == language.to_s }
+
+      # Only take in consideration relevant skills
+      if !top_skill_repo.nil? && (repos.count >= 2 || top_skill_repo.fetch(:popularity) >= 3)
+        dev_skill = DeveloperSkill.find_or_initialize_by(skill_id: skill.id, developer_id: developer.id)
+        dev_skill.code_example = top_skill_repo.fetch(:name)
+        dev_skill.strength = repos.count
+        dev_skill.save
+      end
+    end
+  end
 end
--- lib/recruiter_extensions/update_developer_from_github.rb
+++ lib/recruiter_extensions/update_developer_from_github.rb
@@ -18,20 +18,7 @@ module RecruiterExtensions
                                    needs_update_activity: false)
       end
 
-      candidate.languages.each do |language, repos|
-        repos = repos.map(&:symbolize_keys)
-        skill = Skill.find_or_create_by(name: language.to_s)
-        sorted_repos = repos.sort { |a,b| b[:popularity] <=> a[:popularity] }
-        top_skill_repo = sorted_repos.detect { |repo| repo.fetch(:main_language).to_s == language.to_s }
-
-        # Only take in consideration relevant skills
-        if !top_skill_repo.nil? && (repos.count >= 2 || top_skill_repo.fetch(:popularity) >= 3)
-          dev_skill = DeveloperSkill.find_or_initialize_by(skill_id: skill.id, developer_id: developer.id)
-          dev_skill.code_example = top_skill_repo.fetch(:name)
-          dev_skill.strength = repos.count
-          dev_skill.save
-        end
-      end
+      DeveloperSkill.create_from_candidate(developer: developer, candidate: candidate)
 
       if parse_options.fetch(:parse_contributions)
         candidate.contributions.each do |org_contributions|
