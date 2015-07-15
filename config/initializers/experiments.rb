40f0b294f349b200bb4b0ec6ad9e11269edff369
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-07-15 23:49:34 +0100

    Remove experiments, add tracking and extend unicorn timeout

--- config/initializers/experiments.rb
+++
@@ -1,6 +0,0 @@
-recruiter_client = Recruiter::API.build_client(configuration: {
-      access_token: ENV.fetch('GITHUB_ACCESS_TOKEN') })
-redis_client = Redis.new
-
-ExperimentConfiguration = OpenStruct.new(redis_client: redis_client,
-  recruiter_client: recruiter_client)
