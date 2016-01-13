679106877b3ea3aa1b0042f7d41c1c0c8d3a80ab
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-01-13 15:37:06 +0000

    Small fix on the interesting code examples

--- app/helpers/developer_helper.rb
+++ app/helpers/developer_helper.rb
@@ -4,4 +4,10 @@ module DeveloperHelper
       "#{dev_skill.skill.name}: #{dev_skill.strength}"
     end.join(', ')
   end
+
+  def link_to_code_example(developer_skill)
+    github_login = developer_skill.developer.login
+    project_name = developer_skill.code_example.gsub(/\A#{github_login}\//, '')
+    "https://github.com/#{github_login}/#{project_name}"
+  end
 end
