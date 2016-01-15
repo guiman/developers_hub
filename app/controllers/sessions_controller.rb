75e3f04dc02230fd68e4c4978ae5092702f264ea
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-01-15 18:28:09 +0000

    Clear session on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def logout
     session[:developer_id] = nil
     session[:recruiter_id] = nil
+    session["current_search"] = {}
     redirect_to root_path
   end
 end
